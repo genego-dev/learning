1fb33eab8023f0369db47c5315f2ae3bed7b6fd6
genego-dev <me@edwingenego.com>
2021-02-17 20:51:38 +0100

    update

--- async/isbn.py
+++ async/isbn.py
@@ -1,15 +1,17 @@
-import requests
+import asyncio
+import httpx
 import time
 
 
-def print_book_name(index: int, isbn: int) -> str:
-    response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}")
-    response_dict = response.json()
-    title = response_dict["items"][0]["volumeInfo"]["title"]
-    print(f"{index}: {title}")
+async def get_book_name(index: int, isbn: int):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}")
+        response_dict = response.json()
+        title = response_dict["items"][0]["volumeInfo"]["title"]
+        print(f"{index}: {title}")
 
 
-def main():
+async def main():
     isbn_list = [
         9780007355143,
         9780008108298,
@@ -18,15 +20,15 @@ def main():
         9780316095860,
         9780930289232,
         9780486415871,
-        9780765350381,
     ]
 
-    start_time = time.monotonic()
+    task_list = []
     for index, isbn in enumerate(isbn_list):
-        print_book_name(index, isbn)
+        task_list.append(get_book_name(index, isbn))
+    await asyncio.gather(*task_list)
 
-    print(f"Time Taken:{time.monotonic() - start_time}")
 
-d
 if __name__ == "__main__":
-    main()
+    start_time = time.monotonic()
+    asyncio.run(main())
+    print(f"Time Taken:{time.monotonic() - start_time}")
