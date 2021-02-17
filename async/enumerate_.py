627a367ebc4830b104b3e824479246dfdd1b154c
genego-dev <me@edwingenego.com>
2021-02-17 21:41:42 +0100

    enumerate

--- async/enumerate_.py
+++ async/enumerate_.py
@@ -1,4 +1,7 @@
+import time
+
 test_list = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Okt', 'Nov', 'Dec']
 
 for index, month in enumerate(test_list):
+    time.sleep(0.5)
     print(index, month)
